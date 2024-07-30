9898992ea39253353d2da0514e25c1f137f856ae
Moodinocode <mehdi21092005@gmail.com>
2024-07-30 18:36:00 +0300

    psql logger turned off

--- app2/server/middleware/loggerMiddleware.js
+++ app2/server/middleware/loggerMiddleware.js
@@ -8,17 +8,17 @@ import pool from '../config/db.js'
 const logger = expressWinston.logger({
   transports: [
     new transports.Console(),
-    PostgresTransport({
-      pool,
-      tableName: 'logs',
-      formatLog: (log) => ({
-        level: log.level,
-        message: log.message,
-        meta: log.meta,
-        timestamp: log.timestamp,
-        user_id: log.meta.user_id
-      })
-    })
+    // PostgresTransport({
+    //   pool,
+    //   tableName: 'logs',
+    //   formatLog: (log) => ({
+    //     level: log.level,
+    //     message: log.message,
+    //     meta: log.meta,
+    //     timestamp: log.timestamp,
+    //     user_id: log.meta.user_id
+    //   })
+    // })
   ],
   format: format.combine(
     format.json(),
